14f28a38131281b2d053b184b741c29b4fff139c
Călin-Constantin Popa <calinpopaconstantin@gmail.com>
2024-05-16 18:08:20 +0300

    account menu added + homepage

---
+++ my-app/src/AccountMenu/AccountMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './AccountMenu.scss'
+
+export function AccountMenu() {
+    return (
+
+        <div className="accountMenu">
+            <div className="image">
+                <div className="account--content">
+                    <ul>
+                        <li>Option1</li>
+                        <li>Option2</li>
+                        <li>Option3</li>
+                        <li>Option4</li>
+                        <li>Option5</li>
+                    </ul>
+                </div >
+            </div>
+        </div>
+    )
+}
--- my-app/src/Menu/Menu.js
+++ my-app/src/Menu/Menu.js
@@ -1,19 +1,24 @@
 import React from "react";
 import styles from './Menu.scss';
 import { Button } from "../Button/Button";
+import { AccountMenu } from "../AccountMenu/AccountMenu.js"
 
 
 export function Menu() {
     return (
-        <>
-            <div className='menu'> <ul>
-                <li><Button btnValue={'BtnMenu1'}/></li>
-                <li><Button btnValue={'BtnMenu2'}/></li>
-                <li><Button btnValue={'BtnMenu3'}/></li>
-                <li><Button btnValue={'BtnMenu4'}/></li>
-                <li><Button btnValue={'BtnMenu5'}/></li>
-                <li><Button btnValue={'BtnMenu6'}/></li>
-            </ul></div>
-        </>
+
+        <div className='menu'>
+            <div className="container">
+                <ul>
+                    <li><Button btnValue={'BtnMenu1'} /></li>
+                    <li><Button btnValue={'BtnMenu2'} /></li>
+                    <li><Button btnValue={'BtnMenu3'} /></li>
+                    <li><Button btnValue={'BtnMenu4'} /></li>
+                    <li><Button btnValue={'BtnMenu5'} /></li>
+                    <li><Button btnValue={'BtnMenu6'} /></li>
+                </ul>
+                <AccountMenu />
+            </div>
+        </div>
     )
 }
